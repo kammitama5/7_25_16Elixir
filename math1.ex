a339c0ddfa26cbac6d30ca7d294592db4ee5fe19
kammitama5 <krystal.maughan@gmail.com>
2016-07-26 11:20:05 -0700

    fourth commit

---
+++ math1.ex
@@ -0,0 +1,12 @@
+defmodule Math1 do
+    def sum(a, b) do
+        a + b
+    end
+end
+
+IO.puts Math1.sum(1, 2)
+IO.puts Math1.sum(1.0, 2)
+#IO.puts Math1.sum("hello ", 2)
+#IO.puts Math1.sum("hello ", " world")
+#IO.puts Math1.sum((1/3), 2)
+#IO.puts Math1.sum(1, 55)
